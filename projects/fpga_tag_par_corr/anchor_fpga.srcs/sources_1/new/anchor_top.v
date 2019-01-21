89e8f7eb49bf1e06f06bc3c35ccd7704ee61e8ab
Frank Liu <frank.zijie@gmail.com>
2019-01-21 23:21:41 +0800

    Minor changes.

--- projects/fpga_tag_par_corr/anchor_fpga.srcs/sources_1/new/anchor_top.v
+++ projects/fpga_tag_par_corr/anchor_fpga.srcs/sources_1/new/anchor_top.v
@@ -23,7 +23,7 @@ module anchor_top #(
 
   // parameters
 
-  parameter   NUM_TAGS = 1,
+  parameter   NUM_TAGS = 12,
   parameter   NUM_CHANNELS = 4,
   parameter   PRECISION = 6,
   parameter   CORR_OFFSET = 0,
